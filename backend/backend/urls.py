b57038de11225614621144234ff2a21fb7ed1827
Bhaskar Neupane <bhaskar.neupane.58@gmail.com>
2021-12-17 15:12:58 +0545

    Location data API complete !!

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -14,8 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/location/', include('location_manager.urls')),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
---
+++ backend/location_manager/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+
+from location_manager import views
+
+urlpatterns = [
+    path("location-list/", views.location_list, name="locations")
+]
