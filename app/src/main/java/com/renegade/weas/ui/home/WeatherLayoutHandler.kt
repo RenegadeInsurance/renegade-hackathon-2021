d12482e4fd98a844f2550f1e678ce468a0cee9c1
PhoenixIgris <sachitashrestha1234@gmail.com>
2021-12-18 23:43:50 +0545

    weather layout commit

---
+++ app/src/main/java/com/renegade/weas/ui/home/WeatherLayoutHandler.kt
@@ -0,0 +1,13 @@
+package com.renegade.weas.ui.home
+
+import android.os.SystemClock
+import com.renegade.weas.databinding.WeatherLayoutBinding
+
+class WeatherLayoutHandler {
+    private lateinit var binding :WeatherLayoutBinding
+   fun updateValues(){
+       dateInString = date.toString("yyyy/MM/dd HH:mm:ss")     val date = getCurrentDateTime()
+       val
+
+   }
+}
