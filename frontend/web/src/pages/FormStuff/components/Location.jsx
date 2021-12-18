74b98af79dba3f0730be7895d458d870167b75ee
Aakash Basnet <iamaakashbasnet@gmail.com>
2021-12-19 01:43:04 +0545

    Location fields from text fields to option fields

--- frontend/web/src/pages/FormStuff/components/Location.jsx
+++ frontend/web/src/pages/FormStuff/components/Location.jsx
@@ -1,4 +1,4 @@
-import { Box, Grid, Typography, TextField } from '@mui/material';
+import { Box, Grid, Typography, TextField, Autocomplete } from '@mui/material';
 
 const Location = () => {
   return (
@@ -9,28 +9,34 @@ const Location = () => {
       <Box>
         <Grid container spacing={2}>
           <Grid item xs={12} md={6}>
-            <TextField
-              type='text'
-              label='Country'
-              variant='outlined'
-              size='small'
+            <Autocomplete
+              disablePortal
+              options={country}
+              sx={{ width: 300 }}
+              renderInput={(params) => (
+                <TextField {...params} label='Country' size='small' />
+              )}
             />
           </Grid>
           <Grid item xs={12} md={6}>
-            <TextField
-              type='text'
-              label='State'
-              variant='outlined'
-              size='small'
+            <Autocomplete
+              disablePortal
+              options={state}
+              sx={{ width: 300 }}
+              renderInput={(params) => (
+                <TextField {...params} label='State' size='small' />
+              )}
             />
           </Grid>
 
           <Grid item md={12}>
-            <TextField
-              type='text'
-              label='City'
-              variant='outlined'
-              size='small'
+            <Autocomplete
+              disablePortal
+              options={city}
+              sx={{ width: 300 }}
+              renderInput={(params) => (
+                <TextField {...params} label='City' size='small' />
+              )}
             />
           </Grid>
         </Grid>
@@ -38,5 +44,36 @@ const Location = () => {
     </>
   );
 };
+
+// Top 100 films as rated by IMDb users. http://www.imdb.com/chart/top
+const state = [
+  { label: 'State 1' },
+  { label: 'State 2' },
+  { label: 'State 3' },
+  { label: 'State 4' },
+  { label: 'State 5' },
+  { label: 'State 6' },
+  { label: 'State 7' },
+];
+
+const country = [
+  { label: 'Country 1' },
+  { label: 'Country 2' },
+  { label: 'Country 3' },
+  { label: 'Country 4' },
+  { label: 'Country 5' },
+  { label: 'Country 6' },
+  { label: 'Country 7' },
+];
+
+const city = [
+  { label: 'City 1' },
+  { label: 'City 2' },
+  { label: 'City 3' },
+  { label: 'City 4' },
+  { label: 'City 5' },
+  { label: 'City 6' },
+  { label: 'City 7' },
+];
 
 export default Location;
