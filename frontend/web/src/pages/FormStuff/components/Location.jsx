ba46d979ace42d963872065ba67b8d202596157b
Aakash Basnet <iamaakashbasnet@gmail.com>
2021-12-19 01:02:33 +0545

    Personal detail and Location form added

--- frontend/web/src/pages/FormStuff/components/Location.jsx
+++ frontend/web/src/pages/FormStuff/components/Location.jsx
@@ -1,7 +1,40 @@
+import { Box, Grid, Typography, TextField } from '@mui/material';
+
 const Location = () => {
   return (
     <>
-      <h1>Location Detail</h1>
+      <Typography variant='h4' fontWeight='bold' mb={2}>
+        Location
+      </Typography>
+      <Box>
+        <Grid container spacing={2}>
+          <Grid item xs={12} md={6}>
+            <TextField
+              type='text'
+              label='Country'
+              variant='outlined'
+              size='small'
+            />
+          </Grid>
+          <Grid item xs={12} md={6}>
+            <TextField
+              type='text'
+              label='State'
+              variant='outlined'
+              size='small'
+            />
+          </Grid>
+
+          <Grid item md={12}>
+            <TextField
+              type='text'
+              label='City'
+              variant='outlined'
+              size='small'
+            />
+          </Grid>
+        </Grid>
+      </Box>
     </>
   );
 };
