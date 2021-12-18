63f1963bcf8e5acab9dd9e140447d46d1c5c4a7c
Bhaskar Neupane <bhaskar.neupane.58@gmail.com>
2021-12-18 15:16:58 +0545

    CORS issues fixed

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -40,12 +40,14 @@ INSTALLED_APPS = [
     'location_manager.apps.LocationManagerConfig',
     'risk_management.apps.RiskManagementConfig',
     'weather_manager.apps.WeatherManagerConfig',
-    'rest_framework'
+    'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -123,3 +125,5 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOW_ALL_ORIGINS = True
