6bd609ec4f874b1c1a45780c56465303e578f941
em-azhar <42h4r.4l1@gmail.com>
2021-12-18 23:41:50 +0545

    offline help

---
+++ frontend/mobile/lib/pages/offlineRequest.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+class OfflineRequest extends StatefulWidget {
+  const OfflineRequest({Key? key}) : super(key: key);
+
+  @override
+  _OfflineRequestState createState() => _OfflineRequestState();
+}
+
+class _OfflineRequestState extends State<OfflineRequest> {
+  var _locationMessage = "";
+
+  void getCurrentLocation() async {
+    var position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high);
+    var lastPosition = await Geolocator.getLastKnownPosition();
+    setState(() {
+      _locationMessage = "$position.latitude, $position.longitude";
+    });
+
+    launch('sms:+9779818634306?body=$lastPosition');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          const Icon(
+            Icons.help,
+            size: 46,
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          TextButton(
+            child: const Text(
+              'Ask Help!',
+              style: TextStyle(
+                fontSize: 40,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            onPressed: () {
+              getCurrentLocation();
+            },
+          ),
+          const Text(
+            '(+9779800000000)',
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ frontend/mobile/lib/widget/webviewWidget.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+
+class WebViewWidget extends StatefulWidget {
+  const WebViewWidget({ Key? key }) : super(key: key);
+
+  @override
+  _WebViewWidgetState createState() => _WebViewWidgetState();
+}
+
+class _WebViewWidgetState extends State<WebViewWidget> {
+  late WebViewController controller;
+  double progress = 0;
+  @override
+  Widget build(BuildContext context) {
+    return WillPopScope(
+      onWillPop: () async {
+        if (await controller.canGoBack()) {
+          controller.goBack();
+        }
+        return false;
+      },
+      child: Column(
+        children: [
+          LinearProgressIndicator(
+            value: progress,
+            color: Colors.black,
+            backgroundColor: Colors.white,
+          ),
+          Expanded(
+            child: WebView(
+              initialUrl: 'https://www.bennish.net/web-notifications.html',
+              javascriptMode: JavascriptMode.unrestricted,
+              onWebViewCreated: (controller) => {this.controller = controller},
+              onProgress: (progress) =>
+                  setState(() => this.progress = progress / 100),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
