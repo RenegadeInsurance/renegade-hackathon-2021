38a15dbf88b0c12713254032a8f092be7e8107f4
Bikram Lama <thebikramlama1123@gmail.com>
2021-12-19 03:08:45 +0545

    Added api

--- astranepalko/backend/Modules/Response/Http/Controllers/Api/ResponseController.php
+++ astranepalko/backend/Modules/Response/Http/Controllers/Api/ResponseController.php
@@ -2,78 +2,17 @@
 
 namespace Modules\Response\Http\Controllers\Api;
 
-use Illuminate\Contracts\Support\Renderable;
-use Illuminate\Http\Request;
-use Illuminate\Routing\Controller;
+use Modules\Response\Entities\Response;
+use Modules\Core\Http\Controllers\BaseController;
+use Modules\Response\Repositories\ResponseRepository;
 
-class ResponseController extends Controller
+class ResponseController extends BaseController
 {
-    /**
-     * Display a listing of the resource.
-     * @return Renderable
-     */
-    public function index()
+    public function __construct(Response $response, ResponseRepository $response_repository)
     {
-        return view('response::index');
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     * @return Renderable
-     */
-    public function create()
-    {
-        return view('response::create');
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     * @param Request $request
-     * @return Renderable
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Show the specified resource.
-     * @param int $id
-     * @return Renderable
-     */
-    public function show($id)
-    {
-        return view('response::show');
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     * @param int $id
-     * @return Renderable
-     */
-    public function edit($id)
-    {
-        return view('response::edit');
-    }
-
-    /**
-     * Update the specified resource in storage.
-     * @param Request $request
-     * @param int $id
-     * @return Renderable
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     * @param int $id
-     * @return Renderable
-     */
-    public function destroy($id)
-    {
-        //
+        $this->model = $response;
+        $this->model_name = "Response";
+        $this->repository = $response_repository;
+        parent::__construct();
     }
 }
--- astranepalko/backend/Modules/Response/Routes/api.php
+++ astranepalko/backend/Modules/Response/Routes/api.php
@@ -1,18 +1,9 @@
 <?php
 
-use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
-Route::middleware('auth:api')->get('/response', function (Request $request) {
-    return $request->user();
-});
+Route::name("api.")
+    // ->middleware(["auth:api"])
+    ->group(function () {
+        Route::resource("responses", Api\ResponseController::class);
+    });
