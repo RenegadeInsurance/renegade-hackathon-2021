2d9804589c5ca0dfda9f20388863531dc1ca6b80
Aakash Basnet <iamaakashbasnet@gmail.com>
2021-12-19 09:04:44 +0545

    API route

--- frontend/web/src/pages/FormStuff/components/RiskAssessment.jsx
+++ frontend/web/src/pages/FormStuff/components/RiskAssessment.jsx
@@ -1,4 +1,5 @@
 const RiskAssessment = ({ formData, handleFormData }) => {
+  //http://localhost:8000/api/risk-management/get-questions-dict/flooding/
   return (
     <>
       <h1>Risk Assessment Detail</h1>
